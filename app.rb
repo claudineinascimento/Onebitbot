6700e7dac6fa1e0ae013794e05b880a8da65efcb
claudineinascimento <claudinei.nascimento@gmail.com>
2019-06-06 15:16:58 -0300

    inicio projeto

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'json'
+require 'sinatra'
+require 'sinatra/activerecord'
+  
+require './config/database'
+
+# Load Models
+Dir["./app/models/*.rb"].each {|file| require file }
+  
+class App < Sinatra::Base
+  get '/' do
+    'Hello world!'
+  end
+end
